041cbff7be1635821bc4b6e3b9c1e4c8aefc77cd
Gmarn <marn.greg@gmail.com>
2019-12-18 17:34:37 -0300

    Solution for day 12

---
+++ D12/D12-1.rb
@@ -0,0 +1,77 @@
+file = File.read('input.txt')
+array = file.split("\n")
+array.map! { |a| a.split(',') }
+moons = array.each_with_object([]) do |coord, acc|
+  acc << { x: coord[0].split('=')[1].to_i,
+           y: coord[1].split('=')[1].to_i,
+           z: coord[2].split('=')[1].to_i }
+end
+
+def apply_gravity_on_coord(coord1, coord2, velocities, i, j, axis)
+  if coord1 < coord2
+    velocities[i][axis] += 1
+    velocities[j + i + 1][axis] -= 1
+  elsif coord1 > coord2
+    velocities[i][axis] -= 1
+    velocities[j + i + 1][axis] += 1
+  end
+  velocities
+end
+
+def apply_gravity(coordinates, velocities)
+  coordinates.each_with_index do |coord, index|
+    coordinates[index + 1..-1].each_with_index do |c, jndex|
+      velocities = apply_gravity_on_coord(coord[:x], c[:x], velocities, index, jndex, :x)
+      velocities = apply_gravity_on_coord(coord[:y], c[:y], velocities, index, jndex, :y)
+      velocities = apply_gravity_on_coord(coord[:z], c[:z], velocities, index, jndex, :z)
+    end
+  end
+  velocities
+end
+
+def apply_velocity(coordinates, velocities)
+  new_coord = []
+  coordinates.each_with_index do |coord, index|
+    new_coord << { x: coord[:x] + velocities[index][:x],
+                   y: coord[:y] + velocities[index][:y],
+                   z: coord[:z] + velocities[index][:z] }
+  end
+  new_coord
+end
+
+def run_step(coords, vels)
+  new_vels = apply_gravity(coords, vels)
+  new_coords = apply_velocity(coords, new_vels)
+  return [new_coords, new_vels]
+end
+
+def energy(points)
+  points.each_with_object([]) do |c, acc|
+    acc << c[:x].abs + c[:y].abs + c[:z].abs
+  end
+end
+
+def total_energy(potential, kinetic)
+  nrj = 0
+  (0..potential.length - 1).each do |index|
+    nrj += potential[index] * kinetic[index]
+  end
+  nrj
+end
+
+vel = []
+(moons.length).times { vel << { x: 0, y: 0, z: 0 } }
+
+(1000).times do
+  result = run_step(moons, vel)
+  moons = result[0]
+  vel = result[1]
+end
+
+
+puts moons
+puts 111
+puts vel
+
+result_total = total_energy(energy(moons), energy(vel))
+puts result_total
